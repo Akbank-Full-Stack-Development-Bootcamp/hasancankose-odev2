5c70f92dda6e980848de999f91b7504a1c0be0d2
Hasan Can Kose <hasancannkosee@gmail.com>
2021-06-13 18:56:13 +0300

    Add a middleware to check if IP address is forbidden

---
+++ middleware/middleware/Infrastructures/CheckForIPAddress.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace middleware.Infrastructures
+{
+    public class CheckForIPAddress
+    {
+        private RequestDelegate requestDelegate;
+
+        public CheckForIPAddress(RequestDelegate requestDelegate)
+        {
+            this.requestDelegate = requestDelegate;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var ip = httpContext.Connection.RemoteIpAddress.ToString();
+            httpContext.Items["ip"] = ip;
+
+            await requestDelegate.Invoke(httpContext);
+        }
+    }
+}
